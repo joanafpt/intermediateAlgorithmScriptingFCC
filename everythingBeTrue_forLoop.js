d18dd964a52f26a3ce8a78d0926ab1006a5f2c1f
Joana <joanajteixeira@outlook.com>
2019-04-30 13:26:55 +0100

    //Intermediate Algorithm Scripting: Everything Be True
    /* Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
    
    In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
    
    In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
    
    Remember, you can access object properties through either dot notation or [] notation.
    
    Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.*/

---
+++ everythingBeTrue_forLoop.js
@@ -0,0 +1,25 @@
+//Intermediate Algorithm Scripting: Everything Be True
+
+/* Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot notation or [] notation.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.*/
+
+
+function truthCheck(collection, pre) {
+    for(var i = 0; i < collection.length; i++){
+    console.log(collection[i][pre]) //male male female female
+      if(Boolean(collection[i][pre]) == false){
+        //tb poderia ser if(!!collection[i][pre] == false)
+        return false;
+      }
+    }
+  return true;
+  }
+  
+  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
